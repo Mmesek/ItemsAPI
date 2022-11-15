9061462b01ecf5b3670993bde81e20e161b0eef8
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-11-15 14:50:09 +0100

    Fix Inventory view

--- SQL/views/inventory_view.sql
+++ SQL/views/inventory_view.sql
@@ -1,10 +1,13 @@
 CREATE OR REPLACE VIEW "Inventory" AS
-SELECT "Owner".id,
-    "Instance".name as item,
+SELECT
+    "Owner".server_id,
+    "Owner".user_id,
+    "Item".name as item,
+    "Item".id as item_id,
+    "Instance".id as instance_id,
     "Character_Inventory".quantity
-    FROM (((("Character_Inventory"
-        JOIN "Instance" ON (("Character_Inventory".instance_id = "Instance".id)))
-        JOIN "Owner" ON (
-            ("Owner".character_id = "Character".id)
-            JOIN "Character" ON ("Character".id = "Character_Inventory".character_id)))) 
-    );
+FROM ("Character_Inventory"
+    JOIN "Instance" ON (instance_id = "Instance".id)
+    JOIN "Item" ON (item_id = "Instance".item_id)
+    JOIN "Owner" ON ("Character_Inventory".character_id = "Owner".character_id)
+);
